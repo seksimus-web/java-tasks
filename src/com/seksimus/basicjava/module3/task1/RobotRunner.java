d7e6f5835a1eab92824d0f1574a527425ccf009a
seksimus-web <seksimus@gmail.com>
2026-07-05 22:20:55 +0300

    task1 третьего модуля

--- src/com/seksimus/basicjava/module3/task1/RobotRunner.java
+++ src/com/seksimus/basicjava/module3/task1/RobotRunner.java
@@ -16,35 +16,99 @@ public class RobotRunner {
     public static void moveRobot(Robot robot, int toX, int toY) {
 
         if (toX > robot.getX()) {
-            turnTo(robot, Direction.RIGHT);
-            moveSteps(robot, toX - robot.getX());
+
+            if (robot.getDirection() == Direction.DOWN ) {
+                robot.turnLeft();
+            } else {
+                while (robot.getDirection() != Direction.RIGHT) {
+                    robot.turnRight();
+                }
+            }
+            int steps = toX - robot.getX();
+            for (int i = 0; i < steps; i++) {
+                robot.stepForward();
+            }
         }
 
         if (toX < robot.getX()) {
-            turnTo(robot, Direction.LEFT);
-            moveSteps(robot, robot.getX() - toX);
+
+            if (robot.getDirection() == Direction.UP) {
+                robot.turnLeft();
+            } else {
+                while (robot.getDirection() != Direction.LEFT) {
+                    robot.turnRight();
+                }
+            }
+
+            int steps = robot.getX() - toX;
+            for (int i = 0; i < steps; i++) {
+                robot.stepForward();
+            }
         }
 
         if (toY > robot.getY()) {
-            turnTo(robot, Direction.UP);
-            moveSteps(robot, toY - robot.getY());
+
+            if (robot.getDirection() == Direction.LEFT) {
+                robot.turnLeft();
+            } else {
+                while (robot.getDirection() != Direction.UP) {
+                    robot.turnRight();
+                }
+            }
+
+            int steps = toY - robot.getY();
+            for (int i = 0; i < steps; i++) {
+                robot.stepForward();
+            }
         }
 
         if (toY < robot.getY()) {
-            turnTo(robot, Direction.DOWN);
-            moveSteps(robot, robot.getY() - toY);
+
+            if (robot.getDirection() == Direction.RIGHT) {
+                robot.turnLeft();
+            } else {
+                while (robot.getDirection() != Direction.DOWN) {
+                    robot.turnRight();
+                }
+            }
+
+            int steps = robot.getY() - toY;
+            for (int i = 0; i < steps; i++) {
+                robot.stepForward();
+            }
         }
     }
 
-    private static void turnTo(Robot robot, Direction direction) {
-        while (robot.getDirection() != direction) {
-            robot.turnRight();
-        }
-    }
-
-    private static void moveSteps(Robot robot, int steps) {
-        for (int i = 0; i < steps; i++) {
-            robot.stepForward();
-        }
-    }
+//        if (toX > robot.getX()) {
+//            turnTo(robot, Direction.RIGHT);
+//            moveSteps(robot, toX - robot.getX());
+//        }
+//
+//        if (toX < robot.getX()) {
+//            turnTo(robot, Direction.LEFT);
+//            moveSteps(robot, robot.getX() - toX);
+//        }
+//
+//        if (toY > robot.getY()) {
+//            turnTo(robot, Direction.UP);
+//            moveSteps(robot, toY - robot.getY());
+//        }
+//
+//        if (toY < robot.getY()) {
+//            turnTo(robot, Direction.DOWN);
+//            moveSteps(robot, robot.getY() - toY);
+//        }
+//    }
+//
+//    private static void turnTo(Robot robot, Direction direction) {
+//        while (robot.getDirection() != direction) {
+//            robot.turnRight();
+//        }
+//    }
+//
+//    private static void moveSteps(Robot robot, int steps) {
+//        for (int i = 0; i < steps; i++) {
+//            robot.stepForward();
+//        }
+//    }
 }
